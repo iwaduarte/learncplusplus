7c58388a23169bbdf24eed190f4dfd0812e6715d
iwaduarte <iwaduarte@gmail.com>
2023-10-08 19:45:34 -0300

    fix: first example

--- HelloWorld/c17.h
+++ HelloWorld/c17.h
@@ -1,14 +1,21 @@
 #pragma once
 
 #include <array>
+#include <string_view>
+#include <tuple>
+#include <type_traits>
+
 
 namespace a::b::c
 {
-    inline constexpr std::string_view str{ "hello" };
+	inline constexpr std::string_view str{ "hello" };
+
 }
 
-template <typename... T>
-std::tuple<std::size_t, std::common_type_t<T...>> sum(T... args);
 
-
+ template <typename... T>
+ std::tuple<std::size_t, std::common_type_t<T...>> sum(T... args)
+ {
+	 return { sizeof...(T), (args + ...) };
+ }
 
